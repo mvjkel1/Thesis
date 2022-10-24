dd5b51ffe375fed488aad637e72b45ac12570c36
mvjkel1 <mvjkel9@gmail.com>
2022-10-24 20:34:15 +0200

    ET - 10 : error handler

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const catchAsync = require("./../utils/catch.async");
+const AppError = require("./../utils/app.error");
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -12,12 +13,21 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  // Tour.findOne({ _id: req.params.id })
+
+  if (!user) {
+    return next(new AppError("No user found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
