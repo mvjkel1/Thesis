48ed39227a53514470ca89e681c917f5448231d2
mvjkel1 <mvjkel9@gmail.com>
2022-10-30 19:47:26 +0100

    ET-10 : create a group init

--- server/routes/group.routes.js
+++ server/routes/group.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const groupController = require("../controllers/group.controller");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post("/create", groupController.createGroup);
-router.route("/").get(groupController.getAllGroups);
+router
+  .route("/")
+  .get(groupController.getAllGroups)
+  .post(
+    authController.protect,
+    authController.restrictTo("admin", "class-representative"),
+    groupController.createGroup
+  );
 
 module.exports = router;
