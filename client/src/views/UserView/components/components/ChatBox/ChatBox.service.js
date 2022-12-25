e6fba79a9df41887160e664996ac63efa168f274
bogus <bogus.kania@gmail.com>
2022-12-25 14:57:44 +0100

    ET-29: Chat

---
+++ client/src/views/UserView/components/components/ChatBox/ChatBox.service.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+export const getMessages = (id) => API.get(`/message/${id}`);
+
+export const addMessage = (data) => API.post('/message/', data);
+
+export const getUser = (userId) => API.get(`/user/${userId}`);
